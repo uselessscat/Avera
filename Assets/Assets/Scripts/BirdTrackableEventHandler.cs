abaee584b571ab41a4f657f1601a813011720ae6
Arie <arie.cbpro@gmail.com>
2018-11-30 16:15:45 -0300

    Commit inicial

---
+++ Assets/Assets/Scripts/BirdTrackableEventHandler.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class BirdTrackableEventHandler : MonoBehaviour, Vuforia.ITrackableEventHandler {
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start() {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour) {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+            OnTrackingFound();
+        } else {
+            OnTrackingLost();
+        }
+    }
+
+    private void OnTrackingFound() {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents) {
+            component.enabled = true;
+        }
+
+        // Enable colliders:
+        foreach (Collider component in colliderComponents) {
+            component.enabled = true;
+        }
+
+        // disable audio
+        AudioSource[] audioComponents = GetComponentsInChildren<AudioSource>(true);
+        foreach (AudioSource component in audioComponents) {
+            component.enabled = true;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+    }
+
+    private void OnTrackingLost() {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Disable rendering:
+        foreach (Renderer component in rendererComponents) {
+            component.enabled = false;
+        }
+
+        // Disable colliders:
+        foreach (Collider component in colliderComponents) {
+            component.enabled = false;
+        }
+
+        // disable audio
+        AudioSource[] audioComponents =  GetComponentsInChildren<AudioSource>(true);
+        foreach (AudioSource component in audioComponents) {
+            component.enabled = false;
+        }
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+    }
+}
